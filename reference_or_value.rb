c08806e0fc6af28fade48e8b9e0e52e7b55617a5
desmonddai583 <desmonddai583@gmail.com>
2013-12-02 14:42:27 +0800

    Add pass reference or value exercise

---
+++ reference_or_value.rb
@@ -0,0 +1,19 @@
+#1.1 Pass value
+array = [3,1,4,6,2]
+
+def test(arr)
+  arr.sort   #Do not mutate
+end
+
+test array
+p array
+
+#1.2 Pass reference
+array = [3,1,4,6,2]
+
+def test(arr)
+  arr.sort!   #Do mutate
+end
+
+test array
+p array
