db983001a905a4360c9198467a350930320cc5f8
desmonddai583 <desmonddai583@gmail.com>
2013-12-04 12:38:49 +0800

    Add week 1 quiz

---
+++ week1_quiz.rb
@@ -0,0 +1,25 @@
+# Question 1
+puts 1
+
+# Question 2
+puts 'Array is based on index,value while Hash is based on key,value'
+
+# Question 3
+puts '[1, 2, 3, 3]'
+puts '[1, 2, 3]  new array'
+puts '[1, 2, 3] original array'
+
+# Question 4
+puts 'map return value of the method in block while select return the element which method returns true'
+
+# Question 5
+hash = {'name' => 'Desmond', 'age' => 13}
+
+# Question 6
+puts 'no way!'
+
+# Question 7
+puts '4'
+
+# Question 8
+puts "raise an error"
